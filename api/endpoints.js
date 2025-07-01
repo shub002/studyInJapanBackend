576f827439b4b338e48c77bf17cc2c4dc96db878
shub002 <shubham2somawanshi@gmail.com>
2025-07-01 21:50:49 +0530

    updated code

--- api/endpoints.js
+++ api/endpoints.js
@@ -7,7 +7,7 @@
 // https://study-in-japan-backend.vercel.app/contact/get
 
 
-
+// comment added
 
 // Hit end point to export all contact data in excel
 // https://study-in-japan-backend.vercel.app/contact/export
